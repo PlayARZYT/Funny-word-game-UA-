3a3081d85c1b94c5856b16ba41e0316e343d613d
PlayARZYT <arsenii.o.zaika@gmail.com>
2026-02-16 19:57:22 +0100

    Create gameUA.py
    first version

---
+++ gameUA.py
@@ -0,0 +1,8 @@
+# codehs.com or online-python.com or any .py manager is needed to run!
+noun = input("Напишіть іменник: ")
+color = input("Напишіть колір: ")
+verb = input("Напишіть дієслово: ")
+animal = input("Напишіть тварину: ")
+place = input("Напишіть місце: ")
+text = f"Одного разу {noun} був дуже сумний, тому що він був {color}. \nВін вирішив {verb} у {place}, де зустрів {animal}. \nРазом вони вирішили залишитися в {place} назавжди, \nі стали найкращими друзями."
+print(text)
